4fb4e1d048657f7411bf47e6757a28b005a0d5cb
fetw882 <121428752+fetw882@users.noreply.github.com>
2023-05-25 15:12:02 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['抢在其他人之前了解 AI 的最新发展🎉', '', '', '🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['抢在其他人之前了解 AI 的最新发展🎉', '', '', ''], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
